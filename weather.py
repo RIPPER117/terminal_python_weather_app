5a3a6e2b3acedaabb26049266b9eeb21f8b3879e
eltri <shakirck333@gmail.com>
2019-01-12 09:23:45 +0530

    implemented a weather api using python3 and openwathermap.com

---
+++ weather.py
@@ -0,0 +1,12 @@
+import requests
+
+i=0
+city = input("City Name")
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=0c42f7f6b53b244c78a418f4f181282a' .format(city)
+
+
+json_data =requests.get(url).json()
+data_new=json_data['weather'][0]["main"] ,json_data['weather'][0]["description"]
+
+for i in  data_new:
+	print (i)
